4b0272ebb7934d668872b0d919f3d9d8fb4ac37a
El Elie Polanco Rosario <elie2in1@outlook.com>
2023-10-15 19:26:24 -0400

    kk

---
+++ numeros&cifras/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Confeccionar un programa que permita cargar
+un número entero positivo de hasta tres cifras y 
+muestre un mensaje indicando si tiene 1, 2, o 3 cifras. 
+Mostrar un mensaje de error si el número de cifras es mayor.*/
+
+int num = 0;
+
+string linea=string.Empty;
+Console.WriteLine("inserte un numero para analizar");
+linea = Console.ReadLine();
+num = Convert.ToInt32(linea);
+
+if (num < 10 )
+{
+    Console.WriteLine("El numero ingresado es de una cifra");
+}
+else if (num > 9 && num < 100 )
+{
+    Console.WriteLine("El numero ingresado es de dos cifras");
+}
+ if ( num > 99  && num < 1000 )
+{
+    Console.WriteLine("El numero ingresado es de tres cifras");
+}    
+if (num == 1000 || num > 1000)
+{
+    Console.WriteLine("Error el numero ingresado es Mayor a tres cifras");
+}
